08ee9dcaea2df164a8494f1e12f9a67bce484d2d
Axel Sorensen <axelsorensenwork@gmail.com>
2025-01-13 17:08:11 +0100

    Merging data loading script into main (#12)
    * added README for project description
    
    * added load_data script to data.py

--- src/ehr_classification/data.py
+++ src/ehr_classification/data.py
@@ -1,29 +1,36 @@
 from pathlib import Path
-
+import pdb
 import typer
 from torch.utils.data import Dataset
+import numpy as np
 
+# Define the path to the data directory
+DATA_PATH = "../../data/physion_data"
 
-class MyDataset(Dataset):
-    """My custom dataset."""
+def load_data(data_path: Path, splits: list[int]) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """Load the dataset by combining data from multiple splits."""
+    train_data = np.array([])
+    val_data = np.array([])
+    test_data = np.array([])
+    
+    # Iterate over each split and load the corresponding data
+    for split in splits:
+        train_data_split, val_data_split, test_data_split = load_split_data(split, base_path=data_path)
+        
+        # Concatenate the data from the current split with the previously loaded data
+        train_data = np.concatenate([train_data, train_data_split])
+        val_data = np.concatenate([val_data, val_data_split])
+        test_data = np.concatenate([test_data, test_data_split])
+    
+    return train_data, val_data, test_data
 
-    def __init__(self, raw_data_path: Path) -> None:
-        self.data_path = raw_data_path
+def load_split_data(split_number, base_path='P12data'):
+    """Load data for a specific split."""
+    split_path = f'{base_path}/split_{split_number}'
 
-    def __len__(self) -> int:
-        """Return the length of the dataset."""
-
-    def __getitem__(self, index: int):
-        """Return a given sample from the dataset."""
-
-    def preprocess(self, output_folder: Path) -> None:
-        """Preprocess the raw data and save it to the output folder."""
-
-def preprocess(raw_data_path: Path, output_folder: Path) -> None:
-    print("Preprocessing data...")
-    dataset = MyDataset(raw_data_path)
-    dataset.preprocess(output_folder)
-
-
-if __name__ == "__main__":
-    typer.run(preprocess)
+    # Load the training, validation, and test data for the given split
+    train_data = np.load(f'{split_path}/train_physionet2012_{split_number}.npy', allow_pickle=True)
+    val_data = np.load(f'{split_path}/validation_physionet2012_{split_number}.npy', allow_pickle=True)
+    test_data = np.load(f'{split_path}/test_physionet2012_{split_number}.npy', allow_pickle=True)
+    
+    return train_data, val_data, test_data
